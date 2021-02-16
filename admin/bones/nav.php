6fbb56a2328704502d3a551eb4e66628a311c130
Adam Rajnoha <adam@rajnoha.com>
2021-02-16 12:11:02 +0100

    fixing navigation for admin session

--- admin/bones/nav.php
+++ admin/bones/nav.php
@@ -10,9 +10,13 @@ if ($siteExtraEnabledPages === "1") {
     $nav.=  "<a href='../'>Blog</a>";
 
     foreach ($pageArray as $page) {
-        if ($page !== "../admin" && $page !== "../".$siteBlogPageSlug) {
-            $name = file_get_contents($page."/name",true);
-            $nav .= "<a href='".$page."'>".$name."</a>";
+        if ($page !== "../admin" && $page !== "../".$siteBlogPageSlug && file_exists($page."/name")) {
+                $name = file_get_contents($page."/name",true);
+                if (strpos($name, '</a>') !== false) {
+                    $nav .= $name;
+                } else {
+                    $nav .= "<a href='".$page."'>".$name."</a>";
+                }	
         }
     }
     
